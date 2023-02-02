ba3303a73b4eb9b66798e1cce2c37f330e63f14f
rishikant4 <87231938+rishikant4@users.noreply.github.com>
2023-02-02 20:29:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,8 +2,8 @@ provider "aws" {
     region = "us-east-1"  
 }
 
-resource "aws_instance" "foo" {
-  ami           = "ami-05fa00d4c63e32376" # us-west-2
+resource "aws_instance" "demo" {
+  ami           = "ami-01a4f99c4ac11b03c" # us-west-2
   instance_type = "t2.micro"
   tags = {
       Name = "TF-Instance"
